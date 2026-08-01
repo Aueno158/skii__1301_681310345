d7431bb34b38ef47b9a5b452c3127d2bab8f77bd
GenKenzuto <aueno158@gmail.com>
2026-08-01 12:05:22 +0700

    win

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Player player = other.gameObject.GetComponent<Player>();
+        if (player == null) 
+            return;
+        UIManager.Instance.ShowNotiText($"YOU WIN LMAO \nHP : {player.Hp}");
+    }
+}
